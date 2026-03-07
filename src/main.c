06a1e4ba0371129b187212c622cae985ecf00f8d
Tatu Laras <tatu.laras@gmail.com>
2026-03-07 05:50:24 +0200

    User input, orbital controls

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "cglm/cglm.h"
 #include "gapi.h"
 #include "gapi_types.h"
+#include "grid.h"
 #include "model_loading.h"
+#include "orbital_controls.h"
 #include "stb_image.h"
+#include "user_input.h"
 #include "utility_macros.h"
 
+#include "user_input.h"
 #include <stdlib.h>
 #include <time.h>
 
@@ -15,9 +19,6 @@
 
 const char shader_code[] = {
 #embed "../build/shaders/shader.spv"
-};
-const char shader_2_code[] = {
-#embed "../build/shaders/shader2.spv"
 };
 
 int main(void) {
@@ -30,20 +31,22 @@ int main(void) {
         .shader_count = 1,
     };
 
-    GAPI_ERR(gapi_init(&init_info));
+    GLFWwindow *window;
+    GAPI_ERR(gapi_init(&init_info, &window));
+    uin_init(window);
 
-    GapiPipelineHandle shader;
-    GapiPipelineHandle shader2;
-    GapiPipelineCreateInfo shader_create_info = {
+    GapiPipelineHandle pipeline;
+    GapiPipelineCreateInfo pipeline_create_info = {
         .shader_code = shader_code,
         .shader_code_size = sizeof shader_code,
         .alpha_blending_mode = GAPI_ALPHA_BLENDING_BLEND,
     };
-    GAPI_ERR(gapi_shader_create(&shader_create_info, &shader));
+    GAPI_ERR(gapi_pipeline_create(&pipeline_create_info, &pipeline));
 
-    shader_create_info.shader_code = shader_2_code;
-    shader_create_info.shader_code_size = sizeof shader_2_code;
-    GAPI_ERR(gapi_shader_create(&shader_create_info, &shader2));
+    GapiPipelineHandle grid_pipeline;
+    GapiObjectHandle grid_object;
+    GAPI_ERR(grid_pipeline_create(&grid_pipeline));
+    GAPI_ERR(grid_object_create(100, 1.0, &grid_object));
 
     MLD_ERR(mld_init());
 
@@ -91,17 +94,43 @@ int main(void) {
     vec3 up = {0, 1, 0};
     vec3 right = {1, 0, 0};
     GapiCamera camera = {
-        .pos = {0, 6, 6},
-        .target = {6, 0, 6},
+        .pos = {0, 5, 5},
+        .target = {0, 0, 0},
         .up = {0, 1, 0},
         .fov_degrees = 45.0,
+        .near_plane = 0.001,
+        .far_plane = 10000.0,
     };
 
     // TODO: shouldnt have to create the rect
     GapiObjectHandle rect;
-    GAPI_ERR(gapi_rect_create(0, &rect));
+    GAPI_ERR(gapi_rect_create(barrel_texture, &rect));
 
-    while (!gapi_window_should_close()) {
+    double mouse_x, mouse_y;
+    double delta_time;
+
+    while (!gapi_window_should_close(&delta_time)) {
+
+        uin_get_mouse_delta(&mouse_x, &mouse_y);
+
+        if (uin_is_mouse_button_down(UIN_MOUSE_BUTTON_MIDDLE)) {
+            OrbitalMode mode = ORBITAL_NORMAL;
+            if (uin_is_key_down(UIN_KEY_LEFT_SHIFT))
+                mode = ORBITAL_SHIFT_POSITION;
+            if (uin_is_key_down(UIN_KEY_LEFT_CONTROL))
+                mode = ORBITAL_ZOOM;
+            orbital_camera_update(&camera, mouse_x, mouse_y, mode);
+        }
+        if (uin_is_mouse_button_released(UIN_MOUSE_BUTTON_MIDDLE))
+            uin_set_cursor(UIN_CURSOR_NORMAL);
+        if (uin_is_mouse_button_pressed(UIN_MOUSE_BUTTON_MIDDLE))
+            uin_set_cursor(UIN_CURSOR_DISABLED);
+
+        float scroll_amount = uin_get_scroll();
+
+        if (scroll_amount != 0.0) {
+            orbital_camera_update_zoom(&camera, scroll_amount);
+        }
 
         uint32_t window_width, window_height;
         gapi_get_window_size(&window_width, &window_height);
@@ -113,40 +142,44 @@ int main(void) {
             current_time.tv_sec + current_time.tv_nsec / 1000000000.0;
         float rotation = 2 * M_PI * (fmod(current_time_seconds, 6.0) / 6.0);
 
-        camera.pos[2] = rotation;
+        // camera.pos[2] = rotation;
 
         GAPI_ERR(gapi_render_begin(&camera));
 
         for (uint32_t x = 0; x < WIDTH; x++) {
             for (uint32_t y = 0; y < WIDTH; y++) {
                 uint32_t i = y * WIDTH + x;
-                // GapiPipelineHandle shader_to_use =
-                //     (i % 2 == 0) ? shader : shader2;
-                GapiPipelineHandle shader_to_use = shader;
-
                 mat4 matrix;
                 glm_mat4_identity(matrix);
                 glm_translate(matrix, (vec3){x, 0, y});
                 glm_rotate(matrix, rotation * y * 0.2, up);
-                // glm_rotate(matrix, rotation, right);
 
-                gapi_object_draw(barrels[i], shader_to_use, &matrix);
+                gapi_object_draw(
+                    barrels[i], pipeline, &matrix, GAPI_COLOR_WHITE);
 
                 glm_translate(matrix, (vec3){0, 2, 0});
                 glm_scale(matrix, (vec3){0.1, 0.1, 0.1});
-                gapi_object_draw(cubes[i], shader_to_use, &matrix);
+                gapi_object_draw(cubes[i], pipeline, &matrix, GAPI_COLOR_WHITE);
             }
         }
-
         gapi_rect_draw(rect,
                        (Rect2D){.width = window_width / 3,
                                 .height = window_height / 2,
                                 .x = 40,
                                 .y = 40},
-                       (vec4){1.0, 0.0, 0.1, 0.5},
-                       shader2);
+                       GAPI_COLOR_WHITE,
+                       pipeline);
+
+        mat4 grid_matrix;
+        glm_mat4_identity(grid_matrix);
+        glm_translate(grid_matrix, (vec3){-50, 1, -50});
+        gapi_object_draw(grid_object,
+                         grid_pipeline,
+                         &grid_matrix,
+                         (vec4){1.0, 1.0, 1.0, 0.1});
 
         GAPI_ERR(gapi_render_end());
+        uin_refresh();
     }
 
     mld_free();
